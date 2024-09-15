3b52d4cb501014e6420e19c9853320e0d0affabe
arario1538 <arario1538@gmail.com>
2024-09-16 06:48:54 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const editor = document.getElementById('editor');
-const preview = document.getElementById('preview').contentWindow.document;
-
-editor.addEventListener('input', () => {
-    preview.open();
-    preview.write(editor.value);
-    preview.close();
-});
